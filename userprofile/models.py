9f3ce1de5229282b9ad4b25fe233e450b8b1a39e
Ruben Dario Duarte Anaya <ruben.duarte.anaya@gmail.com>
2023-08-10 11:06:47 -0500

    separate vendors from shoppers, add cart button menu, checkout functionality done

--- userprofile/models.py
+++ userprofile/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class Userprofile(models.Model):
     user = models.OneToOneField(User, related_name='userprofile', on_delete=models.CASCADE)
+    is_vendor = models.BooleanField(default=False)
+    
 
     def __str__(self):
         return self.user.username
