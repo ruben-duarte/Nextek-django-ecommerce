99ff18530f733cf8815966775fc80b43acf79557
Ruben Dario Duarte Anaya <ruben.duarte.anaya@gmail.com>
2023-08-17 15:41:37 -0500

    settings production, colors changes, requirements, metadata and robots.txt

--- nextek/urls.py
+++ nextek/urls.py
@@ -19,12 +19,14 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from core.views import frontpage, about
+from django.views.generic.base import TemplateView
 
 
 urlpatterns = [
 
     path('about/', about, name='about'),
     path('admin/', admin.site.urls),
+    path('robots.txt', TemplateView.as_view(template_name='core/robots.txt', content_type = 'text/plain')),
     path('', include('userprofile.urls')),
     path('', include('store.urls')),
     path('', frontpage, name='frontpage'),    
